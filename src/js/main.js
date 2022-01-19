f888ff61729cde49dc2b18f0d55abd71e1db24fe
Vladis macbook <vladuxs1@gmail.com>
2022-01-19 22:03:16 +0200

    lander and use-cases template done

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,7 @@
-const menu = document.getElementById('mobileMenu')
-const burger = document.getElementById('hamburger')
+const menu = document.getElementById("mobileMenu");
+const burger = document.getElementById("hamburger");
 
-burger.addEventListener('click', function(){
-    console.log('niu');
-    this.classList.toggle('is-active')
-    console.log(menu);
-    menu.classList.toggle('active')
-})
-
+burger.addEventListener("click", function () {
+  this.classList.toggle("is-active");
+  menu.classList.toggle("active");
+});
