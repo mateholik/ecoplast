68bad925c3449ae149cf3d58a04a668f64e14dce
Vladis macbook <vladuxs1@gmail.com>
2022-01-23 22:38:58 +0200

    design done

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,23 @@
 const menu = document.getElementById("mobileMenu");
 const burger = document.getElementById("hamburger");
+//TODO dynamic items
+const moreBtn = document.getElementById("more");
+const priceBoxes = document.querySelectorAll("#tables .table-holder");
+const priceArrow = moreBtn.getElementsByTagName("img")[0];
 
 burger.addEventListener("click", function () {
   this.classList.toggle("is-active");
   menu.classList.toggle("active");
+});
+
+moreBtn.addEventListener("click", function () {
+  priceBoxes.forEach((item, index) => {
+    if (index !== 0) {
+      item.classList.toggle("table-holder--show");
+    }
+  });
+  priceArrow.classList.toggle("rotate");
+  document
+    .getElementsByClassName("tables-title")[0]
+    .scrollIntoView({ behavior: "smooth" });
 });
