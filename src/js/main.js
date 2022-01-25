2799a891a36ed6a17865d093c01561ec7c710284
Vladis macbook <vladuxs1@gmail.com>
2022-01-25 23:11:56 +0200

    design done

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,29 @@
-const heroSLider = new Glide("#heroSLider", {
-  type: "carousel",
-  autoplay: 4000,
-  animationDuration: 800,
-});
+if (document.getElementById("heroSLider")) {
+  const heroSLider = new Glide("#heroSLider", {
+    type: "carousel",
+    autoplay: 4000,
+    animationDuration: 800,
+  });
 
-const areasSLider = new Glide("#areasSLider", {
-  type: "carousel",
-  // autoplay: 4000,
-  animationDuration: 800,
-  perView: 3,
-  gap: 20,
-  breakpoints: {
-    900: {
-      perView: 2,
+  const areasSLider = new Glide("#areasSLider", {
+    type: "carousel",
+    autoplay: 4000,
+    animationDuration: 800,
+    perView: 3,
+    gap: 20,
+    breakpoints: {
+      900: {
+        perView: 2,
+      },
+      600: {
+        perView: 1,
+      },
     },
-    600: {
-      perView: 1,
-    },
-  },
-});
+  });
 
-heroSLider.mount();
-areasSLider.mount();
+  heroSLider.mount();
+  areasSLider.mount();
+}
 
 const menu = document.getElementById("mobileMenu");
 const burger = document.getElementById("hamburger");
@@ -30,10 +32,9 @@ burger.addEventListener("click", function () {
   menu.classList.toggle("active");
 });
 
-//TODO dynamic items
-const moreBtn = document.getElementById("more");
+const moreBtn = document.querySelectorAll("#first #more")[0];
 if (moreBtn) {
-  const priceBoxes = document.querySelectorAll("#tables .table-holder");
+  const priceBoxes = document.querySelectorAll("#first #tables .table-holder");
   const priceArrow = moreBtn.getElementsByTagName("img")[0];
   moreBtn.addEventListener("click", function () {
     priceBoxes.forEach((item, index) => {
@@ -44,6 +45,23 @@ if (moreBtn) {
     priceArrow.classList.toggle("rotate");
     document
       .getElementsByClassName("tables-title")[0]
+      .scrollIntoView({ behavior: "smooth" });
+  });
+
+  const moreBtn2 = document.querySelectorAll("#second #more")[0];
+  const priceBoxes2 = document.querySelectorAll(
+    "#second #tables .table-holder"
+  );
+  const priceArrow2 = moreBtn.getElementsByTagName("img")[0];
+  moreBtn2.addEventListener("click", function () {
+    priceBoxes2.forEach((item, index) => {
+      if (index !== 0) {
+        item.classList.toggle("table-holder--show");
+      }
+    });
+    priceArrow2.classList.toggle("rotate");
+    document
+      .getElementsByClassName("tables-title")[1]
       .scrollIntoView({ behavior: "smooth" });
   });
 }
